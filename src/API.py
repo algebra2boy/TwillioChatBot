ae94264c53118cad90284de280f378664b8e98d5
jefzen28 <jefzen28@gmail.com>
2022-08-03 15:12:32 -0400

    Directions api
    don't forget to 'pip install googlemaps'. If there is an error, please let me know

--- src/API.py
+++ src/API.py
@@ -1,20 +1,41 @@
 
 import requests
-import src.messages as m # import all the constants and messages
+import googlemaps
+import src.messages as m  # import all the constants and messages
 
 
-def get_weather_info(latitude:str, longtitude: str, weather_APIKEY: str) -> str:
-	try:
-		weather_info = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longtitude}&appid={weather_APIKEY}")
-		weather_info.raise_for_status()
-		# Code here will only run if the request is successful (200/201)
-		return weather_info.json()
-	except requests.exceptions.HTTPError:
-		return m.weatherNOTFOUND
+def get_weather_info(latitude: str, longitude: str, weather_apikey: str) -> str:
+    try:
+        weather_info = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat="
+                                    f"{latitude}&lon={longitude}&appid={weather_apikey}")
+        weather_info.raise_for_status()
+        # Code here will only run if the request is successful (200/201)
+        return weather_info.json()
+    except requests.exceptions.HTTPError:
+        return m.weatherNOTFOUND
 
 
+<<<<<<< Updated upstream
 
 
 
 
 #TODO
+=======
+def get_address(lat, lon, key):
+    """receives latitude, longitude, and googlemapsapi key to return address of a location"""
+    geo = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lon}&key={key}").json()
+    address_components = geo['results'][0]['address_components']
+    return f"{address_components[0]['long_name']} {address_components[1]['long_name']} " \
+           f"{address_components[2]['long_name']}, {address_components[4]['short_name']}"
+
+
+def get_directions_info(lat, lon, destination, mode, key):
+    gmap = googlemaps.Client(key=key)
+    origin = get_address(lat, lon, key)
+    directions_result = gmap.directions(origin, destination, mode.lower())
+    return f"To get from {origin} to {destination}, it will take {directions_result[0]['legs'][0]['duration']['text']}"\
+        f" ({directions_result[0]['legs'][0]['distance']['text']}) by {mode.lower()}"
+
+# TODO
+>>>>>>> Stashed changes
--- src/userinfo.py
+++ src/userinfo.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class UserInfo:
 
 	def __init__(self, body, name, phone_number, longtitude, latitude, time):
@@ -25,3 +26,36 @@ class UserInfo:
 
 	def has_location(self) -> bool:
 		return self.latitude is not None and self.longtitude is not None 
+=======
+import json
+
+
+class UserInfo:
+
+    def __init__(self, body, name, phone_number, longitude, latitude, time):
+        self.body = body
+        self.name = name
+        self.phone_number = phone_number
+        self.longitude = longitude
+        self.latitude = latitude
+        self.time = time
+        self.option = 0
+
+    def dict_format(self) -> dict:
+        return {"body": self.body,
+                "name": self.name,
+                "phone_number": self.phone_number,
+                "longitude": self.longitude,
+                "latitude": self.latitude,
+                "time received": self.time,
+                "option": self.option}
+
+    def JSON_data(self) -> dict:
+        return {"longitude": self.longitude,
+                "latitude": self.latitude,
+                "time received": self.time,
+                "option": self.option}
+
+    def has_location(self) -> bool:
+        return self.latitude is not None and self.longitude is not None
+>>>>>>> Stashed changes
